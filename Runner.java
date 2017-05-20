d193c05afe771308ce1683d9ec3e38123d3ad65a
Koding <danielsassman@gmail.com>
2017-05-20 22:48:18 +0000

    removing the .javas

--- Runner.java
+++
@@ -1,9 +0,0 @@
-public class Runner{
-
-	public static void main(String[] args){
-		Lambdas testOne = new Lambdas("Daniel", 1983);
-		Lambdas testTwo = new Lambdas("Berkeley", 1981);
-		Lambdas.displayList(Lambdas.list);
-		System.out.println(Lambdas.Sex.MALE);
-	}
-}
